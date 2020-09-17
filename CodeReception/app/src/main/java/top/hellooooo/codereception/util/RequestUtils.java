e24e465bcdfde6564b02007361325d0bc4123e61
Q2035 <2383862181@qq.com>
2020-09-17 14:55:23 +0800

    Add utils

---
+++ CodeReception/app/src/main/java/top/hellooooo/codereception/util/RequestUtils.java
@@ -0,0 +1,9 @@
+package top.hellooooo.codereception.util;
+
+/**
+ * @Author Q
+ * @Date 17/09/2020 14:54
+ * @Description 发送请求到服务端
+ */
+public class RequestUtils {
+}
---
+++ CodeReception/app/src/main/java/top/hellooooo/codereception/util/SMSUtil.java
@@ -0,0 +1,10 @@
+package top.hellooooo.codereception.util;
+
+/**
+ * @Author Q
+ * @Date 17/09/2020 14:54
+ * @Description
+ */
+public class SMSUtil {
+
+}
